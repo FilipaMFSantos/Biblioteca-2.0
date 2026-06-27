7ae4879445cfd15ca1b1ea1a2fe9f919edf221d4
FilipaMFSantos <filipamfsantos@ua.pt>
2026-06-27 17:06:47 +0100

    Add separator parameter to read_csv function

--- source/data.py
+++ source/data.py
@@ -65,6 +65,7 @@ def load_raw(path):
     try:
         df = pd.read_csv(
             path,
+            sep=",",
             encoding="utf-8",
             on_bad_lines="skip",
             dtype={"isbn": "string", "isbn13": "string"},
